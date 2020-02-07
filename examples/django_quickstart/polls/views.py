e62bbda3100e47237fdc372d1e84a7f5e5cf905e
Joshua Mabina <mabinajoshua@gmail.com>
2020-02-07 08:48:46 +0300

    clean up

--- examples/django_quickstart/polls/views.py
+++ examples/django_quickstart/polls/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    context = { 'polls': [] }
-    return render(request, 'polls/index.html', context)
+  context = { 'polls': [] }
+  return render(request, 'polls/index.html', context)
 
 def show(request, question_id):
-    context = { 'poll': {}}
-    return render(request, 'polls/show.html', context)
+  context = { 'poll': {}}
+  return render(request, 'polls/show.html', context)
 
 def store(request):
-    return redirect('polls:index')
+  Question.create(request.POST)
+
+  return redirect('polls:index')
 
